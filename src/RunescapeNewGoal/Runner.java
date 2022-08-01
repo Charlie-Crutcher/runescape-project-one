14e58ff4101bf361e881a2cf26d46f48c1e57b1e
Charlie <charliemarkcrutcher@gmail.com>
2022-08-01 19:15:35 +0100

    Added arrays

--- src/RunescapeNewGoal/Runner.java
+++ src/RunescapeNewGoal/Runner.java
@@ -1,6 +1,9 @@
 package RunescapeNewGoal;
+import java.util.Arrays;
 public class Runner {
     public static void main(String[] args){
+        String[] goalCategories = {"Skilling", "Questing", "Maxing", "Diaries", "Bossing", "Clue Scrolls"};
+
         NextGoal firstGoal = new NextGoal("99 Magic",false);
         NextGoal secondGoal = new NextGoal("Ardougne Hard Diary",false);
         NextGoal thirdGoal = new NextGoal("99 Thieving",false);
@@ -8,5 +11,7 @@ public class Runner {
         firstGoal.goalComplete();
         secondGoal.futureGoals();
         thirdGoal.futureGoals();
+
+        System.out.println("The current goal genres currently consist of: "+Arrays.toString(goalCategories));
     }
 }
