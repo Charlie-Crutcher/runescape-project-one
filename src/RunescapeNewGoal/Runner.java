69f4f0b1d255e72f942da09e7b6e96b1f17814d8
Charlie <109866763+choggly@users.noreply.github.com>
2022-08-12 19:11:19 +0100

    Add files via upload

--- src/RunescapeNewGoal/Runner.java
+++ src/RunescapeNewGoal/Runner.java
@@ -1,12 +1,25 @@
 package RunescapeNewGoal;
+import java.util.Arrays;
+import java.util.Scanner;
 public class Runner {
-    public static void main(String[] args){
-        NextGoal firstGoal = new NextGoal("99 Magic",false);
-        NextGoal secondGoal = new NextGoal("Ardougne Hard Diary",false);
-        NextGoal thirdGoal = new NextGoal("99 Thieving",false);
 
-        firstGoal.goalComplete();
-        secondGoal.futureGoals();
-        thirdGoal.futureGoals();
+    Agility runAgility = new Agility();
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ver: 0.1");
+        System.out.println("Welcome to Charlie's Runescape Goal Tracker & Calculator.");
+        System.out.println("Standby for updates and new versions with updated features and methods.");
+
+        System.out.println("Please use lowercase when entering commands.");
+        System.out.println("Please select the skill you would like to train: ");
+            String initialGoalSelection = input.nextLine();
+            System.out.printf("You Entered "+initialGoalSelection);
+
+        if(initialGoalSelection.equals("agility")){
+            initialGoalSelection.Agility();
+        }
+
+
     }
 }
